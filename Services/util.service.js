e4527247226be89f1c0b5f8d4e74b5d4bafdb5f1
Digidigidoom <tomshahar91@gmail.com>
2025-04-21 18:00:01 +0300

    Add draw to file

--- Services/util.service.js
+++ Services/util.service.js
@@ -6,10 +6,16 @@ export const utilService = {
     httpGet,
     readJsonFile,
     download,
-    writeJsonFile
+    writeJsonFile,
+    getRandomIntInclusive
 }
 
 
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  }
 
 function readJsonFile(path) {
     const str = fs.readFileSync(path, 'utf8')
